305661cdf352ba02bff2cc9730afc0d67e2dc7a4
arissetia1960 <57148887+arissetia1960@users.noreply.github.com>
2021-10-02 15:31:44 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+var iklan = "&lt;script type=&quot;text/javascript&quot;&gt;
+	atOptions = {
+		&apos;key&apos; : &apos;a9e06a70b6ac8d3e7f9f008fa4391242&apos;,
+		&apos;format&apos; : &apos;iframe&apos;,
+		&apos;height&apos; : 250,
+		&apos;width&apos; : 300,
+		&apos;params&apos; : {}
+	};
+	document.write(&apos;&lt;scr&apos; + &apos;ipt type=&quot;text/javascript&quot; src=&quot;http&apos; + (location.protocol === &apos;https:&apos; ? &apos;s&apos; : &apos;&apos;) + &apos;://ascertaincrescenthandbag.com/a9e06a70b6ac8d3e7f9f008fa4391242/invoke.js&quot;&gt;&lt;/scr&apos; + &apos;ipt&gt;&apos;);
+&lt;/script&gt;";
+var iklan2 = "&lt;script type=&quot;text/javascript&quot;&gt;
+	atOptions = {
+		&#39;key&#39; : &#39;f64d9b2f3705b712bc5b315edd23cf54&#39;,
+		&#39;format&#39; : &#39;iframe&#39;,
+		&#39;height&#39; : 60,
+		&#39;width&#39; : 468,
+		&#39;params&#39; : {}
+	};
+	document.write(&#39;&lt;scr&#39; + &#39;ipt type=&quot;text/javascript&quot; src=&quot;http&#39; + (location.protocol === &#39;https:&#39; ? &#39;s&#39; : &#39;&#39;) + &#39;://www.madcpms.com/f64d9b2f3705b712bc5b315edd23cf54/invoke.js&quot;&gt;&lt;/scr&#39; + &#39;ipt&gt;&#39;);
+&lt;/script&gt;";
