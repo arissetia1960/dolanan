44fa1c27575b133e63fb538042d0ab1ebcee7546
arissetia1960 <57148887+arissetia1960@users.noreply.github.com>
2021-10-07 22:20:11 +0700

    Update js.js

--- js.js
+++ js.js
@@ -12,3 +12,7 @@ var iklan2 = function(){
 	document.write('<scr' + 'ipt type="text/javascript" async="async" data-cfasync="false" src="http' + (location.protocol === 'https:' ? 's' : '') + '://ascertaincrescenthandbag.com/8cb5651b182d66e83217f3331900dc23/invoke.js"></scr' + 'ipt>');
 	document.write('<div id="container-8cb5651b182d66e83217f3331900dc23"></div>');
 }
+var iklan3 = function(){
+	document.write('<scr' + 'ipt type="text/javascript" async="async" data-cfasync="false" src="http' + (location.protocol === 'https:' ? 's' : '') + '://ascertaincrescenthandbag.com/e714275550027be2ef6eb61c702a5293/invoke.js"></scr' + 'ipt>');
+	document.write('<div id="container-e714275550027be2ef6eb61c702a5293"></div>');
+}
