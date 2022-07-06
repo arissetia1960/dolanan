ac1f0b4ff11069fb9786e2023c04f83f16ff8dc5
arissetia1960 <57148887+arissetia1960@users.noreply.github.com>
2022-07-06 18:50:02 +0700

    Update js.js

--- js.js
+++ js.js
@@ -1,12 +1,14 @@
 var iklan1 = function(){
-atOptions = {
-		'key' : 'a9e06a70b6ac8d3e7f9f008fa4391242',
+<script type="text/javascript">
+	atOptions = {
+		'key' : '80ca87f8813a501d8aabf7a59fa25265',
 		'format' : 'iframe',
 		'height' : 250,
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://ascertaincrescenthandbag.com/a9e06a70b6ac8d3e7f9f008fa4391242/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://romancemind.com/80ca87f8813a501d8aabf7a59fa25265/invoke.js"></scr' + 'ipt>');
+</script>;
 }
 var iklan2 = function(){
 	document.write('<scr' + 'ipt type="text/javascript" async="async" data-cfasync="false" src="http' + (location.protocol === 'https:' ? 's' : '') + '://ascertaincrescenthandbag.com/8cb5651b182d66e83217f3331900dc23/invoke.js"></scr' + 'ipt>');
