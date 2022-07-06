2e6edd8a599566b65bd1d39c92479dc86ddc67dc
arissetia1960 <57148887+arissetia1960@users.noreply.github.com>
2022-07-06 21:51:56 +0700

    Update js.js

--- js.js
+++ js.js
@@ -6,14 +6,20 @@ atOptions = {
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://ascertaincrescenthandbag.com/a9e06a70b6ac8d3e7f9f008fa4391242/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://romancemind.com/a9e06a70b6ac8d3e7f9f008fa4391242/invoke.js"></scr' + 'ipt>');
 }
 let iklan2 = function(){
-	document.write('<scr' + 'ipt type="text/javascript" async="async" data-cfasync="false" src="http' + (location.protocol === 'https:' ? 's' : '') + '://ascertaincrescenthandbag.com/8cb5651b182d66e83217f3331900dc23/invoke.js"></scr' + 'ipt>');
-	document.write('<div id="container-8cb5651b182d66e83217f3331900dc23"></div>');
+	atOptions = {
+		'key' : '7ca078b544ff0d15bd6e2ac5b236799f',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://romancemind.com/7ca078b544ff0d15bd6e2ac5b236799f/invoke.js"></scr' + 'ipt>');
 }
 let iklan3 = function(){
-	document.write('<scr' + 'ipt type="text/javascript" async="async" data-cfasync="false" src="http' + (location.protocol === 'https:' ? 's' : '') + '://ascertaincrescenthandbag.com/e714275550027be2ef6eb61c702a5293/invoke.js"></scr' + 'ipt>');
-	document.write('<div id="container-e714275550027be2ef6eb61c702a5293"></div>');
+	document.write('<scr' + 'ipt type="text/javascript" async="async" data-cfasync="false" src="http' + (location.protocol === 'https:' ? 's' : '') + '://romancemind.com/6d7791499fbd465d73e0b856cd8aac68/invoke.js"></scr' + 'ipt>');
+	document.write('<div id="container-6d7791499fbd465d73e0b856cd8aac68"></div>');
 }
 
