8bc1e95ff80d67bf47cfba66cc356fe631697a6c
arissetia1960 <57148887+arissetia1960@users.noreply.github.com>
2023-07-09 17:27:47 +0700

    Update js.js

--- js.js
+++ js.js
@@ -26,6 +26,7 @@ let iklan728pertama = function(){
 	atOptions = {
 		'key' : '842fa4960db6bc726f7142d536c56686',
 		'format' : 'iframe',
+		'class' : 'slide',
 		'height' : 90,
 		'width' : 728,
 		'params' : {}
