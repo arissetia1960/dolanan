25e6c324c6eaa89d2e6e0c4d6820685feb6593cf
arissetia1960 <57148887+arissetia1960@users.noreply.github.com>
2024-01-30 11:36:32 +0700

    Create native1.js

---
+++ native1.js
@@ -0,0 +1,3 @@
+// ads.js
+
+document.write('<script async="async" data-cfasync="false" src="//pl18418137.toprevenuegate.com/2e03f5429081687195a9c6131f039e09/invoke.js"></script><div id="container-2e03f5429081687195a9c6131f039e09"></div>');
